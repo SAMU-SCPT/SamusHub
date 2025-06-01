f81f4d6b69b9fbd38c39d7a1348af710625c9b23
GGOP <yagof168@gmail.com>
2025-06-01 18:04:02 -0300

    Update Interface.lua

--- Interface.lua
+++ Interface.lua
@@ -1,5 +1,26 @@
-local Interface = loadstring(game:HttpGet("https://raw.githubusercontent.com/SAMU-SCPT/SamusHub/refs/heads/main/Interface.lua"))()
+local Interface = {}
 
-local Window = Interface:CreateWindow("SAMU HUB")
+local UserInputService = game:GetService("UserInputService")
+local gui = Instance.new("ScreenGui")
+gui.Name = "SAMU_HUB"
+gui.Parent = game.CoreGui
+gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-print("Main.lua executado, janela criada")
+local floatButton = Instance.new("ImageButton")
+floatButton.Name = "FloatButton"
+floatButton.Parent = gui
+floatButton.Size = UDim2.new(0, 50, 0, 50)
+floatButton.Position = UDim2.new(0, 20, 0.5, -25)
+floatButton.BackgroundTransparency = 0.5
+floatButton.Image = "rbxasset://textures/ui/GuiImagePlaceholder.png"
+
+floatButton.MouseButton1Click:Connect(function()
+	print("Botão flutuante clicado")
+end)
+
+function Interface:CreateWindow(titleText)
+	print("Interface carregada: "..titleText)
+	return Interface
+end
+
+return Interface
