8c0a51597db918312b39b5e7d4df993f7349eac1
GGOP <yagof168@gmail.com>
2025-06-01 17:25:47 -0300

    Main.lua

--- Main.lua
+++ Main.lua
@@ -1,44 +1,139 @@
-local Interface = loadstring(game:HttpGet("https://raw.githubusercontent.com/SAMU-SCPT/SamusHub/refs/heads/main/Interface.lua"))()
+local Interface = loadstring(game:HttpGet("https://raw.githubusercontent.com/SAMU-SCPT/SamusHub/main/Interface.lua"))()
 
-local win = Interface:CreateWindow("SAMU HUB - Blox Fruits")
+local Window = Interface:CreateWindow("SAMU HUB")
 
-local farmTab = win:AddTab("AutoFarm")
+local AutoFarmTab = Interface:AddTab("AutoFarm")
+local FruitTab = Interface:AddTab("Frutas")
+local PlayerTab = Interface:AddTab("Player")
+local BountyTab = Interface:AddTab("Bounty")
+local TeleportTab = Interface:AddTab("Teleport")
+local ShopTab = Interface:AddTab("Shop")
+local StatsTab = Interface:AddTab("Stats")
 
-local farmEnabled = false
-local function toggleAutoFarm()
-    farmEnabled = not farmEnabled
-    if farmEnabled then
-        print("AutoFarm ativado")
-    else
-        print("AutoFarm desativado")
-    end
+local function hideAllTabs()
+	for _, tab in pairs({AutoFarmTab, FruitTab, PlayerTab, BountyTab, TeleportTab, ShopTab, StatsTab}) do
+		tab.Visible = false
+	end
 end
 
-Interface:CreateButton("Ativar/Desativar AutoFarm", farmTab, toggleAutoFarm)
+local menu = {
+	{"AutoFarm", AutoFarmTab},
+	{"Frutas", FruitTab},
+	{"Player", PlayerTab},
+	{"Bounty", BountyTab},
+	{"Teleport", TeleportTab},
+	{"Shop", ShopTab},
+	{"Stats", StatsTab}
+}
 
-local fruitTab = win:AddTab("Frutas")
-
-local pickFruits = false
-local autoEat = false
-local autoDrop = false
-
-local function togglePickFruits()
-    pickFruits = not pickFruits
-    print("Pegar frutas soltas:", pickFruits)
+for i, v in ipairs(menu) do
+	local btn = Interface:CreateButton(v[1], Window.Main, function()
+		hideAllTabs()
+		v[2].Visible = true
+	end)
+	btn.Position = UDim2.new(0, 10, 0, 40 + (i - 1) * 35)
 end
 
-local function toggleAutoEat()
-    autoEat = not autoEat
-    print("Comer frutas automaticamente:", autoEat)
-end
+Interface:CreateButton("Start Auto Farm", AutoFarmTab, function()
+	print("Auto Farm ativado")
+end)
 
-local function toggleAutoDrop()
-    autoDrop = not autoDrop
-    print("Dropar frutas automaticamente:", autoDrop)
-end
+Interface:CreateButton("Auto Attack", AutoFarmTab, function()
+	print("Auto Attack ativado")
+end)
 
-Interface:CreateButton("Pegar frutas soltas", fruitTab, togglePickFruits)
-Interface:CreateButton("Comer frutas automaticamente", fruitTab, toggleAutoEat)
-Interface:CreateButton("Dropar frutas automaticamente", fruitTab, toggleAutoDrop)
+Interface:CreateButton("Auto Gun Attack", AutoFarmTab, function()
+	print("Auto Gun Attack ativado")
+end)
 
-print("Interface carregada.")
+Interface:CreateButton("Coletar Frutas no Mapa", FruitTab, function()
+	print("Buscando frutas no mapa")
+end)
+
+Interface:CreateButton("Rodar Fruta Aleatória", FruitTab, function()
+	print("Rodando fruta")
+end)
+
+Interface:CreateButton("Armazenar Fruta", FruitTab, function()
+	print("Armazenando fruta")
+end)
+
+Interface:CreateButton("Comer Fruta", FruitTab, function()
+	print("Comendo fruta")
+end)
+
+Interface:CreateButton("Dropar Fruta", FruitTab, function()
+	print("Dropando fruta")
+end)
+
+Interface:CreateButton("Teleportar para Jogador", PlayerTab, function()
+	print("Teleportando para player")
+end)
+
+Interface:CreateButton("Atacar Jogador", PlayerTab, function()
+	print("Atacando player")
+end)
+
+Interface:CreateButton("Start Bounty Hunt", BountyTab, function()
+	print("Bounty Hunt iniciado")
+end)
+
+Interface:CreateButton("Teleport Sea 1", TeleportTab, function()
+	print("Teleportando para Sea 1")
+end)
+
+Interface:CreateButton("Teleport Sea 2", TeleportTab, function()
+	print("Teleportando para Sea 2")
+end)
+
+Interface:CreateButton("Teleport Sea 3", TeleportTab, function()
+	print("Teleportando para Sea 3")
+end)
+
+Interface:CreateButton("Teleport Ilha 1", TeleportTab, function()
+	print("Teleportando para Ilha 1")
+end)
+
+Interface:CreateButton("Teleport Ilha 2", TeleportTab, function()
+	print("Teleportando para Ilha 2")
+end)
+
+Interface:CreateButton("Comprar Armas", ShopTab, function()
+	print("Comprando armas")
+end)
+
+Interface:CreateButton("Comprar Estilo de Luta", ShopTab, function()
+	print("Comprando estilo de luta")
+end)
+
+Interface:CreateButton("Comprar Fruta (Normal)", ShopTab, function()
+	print("Comprando fruta normal")
+end)
+
+Interface:CreateButton("Comprar Fruta (Avançada)", ShopTab, function()
+	print("Comprando fruta avançada")
+end)
+
+Interface:CreateButton("Auto Status - Fruta", StatsTab, function()
+	print("Auto status em fruta")
+end)
+
+Interface:CreateButton("Auto Status - Espada", StatsTab, function()
+	print("Auto status em espada")
+end)
+
+Interface:CreateButton("Auto Status - Arma", StatsTab, function()
+	print("Auto status em arma")
+end)
+
+Interface:CreateButton("Auto Status - Defesa", StatsTab, function()
+	print("Auto status em defesa")
+end)
+
+Interface:CreateButton("Auto Status - Soco", StatsTab, function()
+	print("Auto status em soco")
+end)
+
+Interface:CreateButton("Auto Status - Balanceado", StatsTab, function()
+	print("Distribuindo status igualmente")
+end)
